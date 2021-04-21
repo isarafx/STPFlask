b8fd48570e7890ed0fe3e5e53f9a02ca7a21abd0
Isarafx <isarawn@gmail.com>
2021-04-21 19:35:40 +0700

    ep5

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    "Create column to store our data"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def home():
