b8ba34124adeaecb70cb72f640e69efca7a69c3a
Isarafx <isarawn@gmail.com>
2021-04-21 18:49:00 +0700

    ep2

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def home():
+
+    name = "Son"
+    return render_template('home.html', name=name)
+
+@app.route('/about')
+def about():
+    return "Hello, about page"
 
 if __name__ == '__main__':
     app.run(debug=True)
